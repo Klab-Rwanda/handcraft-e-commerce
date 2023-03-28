83748ed8556a0e9f2c2e159ffc95ebc5e1dbd453
IshKevin <carterk279@gmail.com>
2023-03-28 21:34:22 +0200

    add venderprofile

--- src/Components/Singlevender/Venderprofile/VenderProfile.js
+++ src/Components/Singlevender/Venderprofile/VenderProfile.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const VenderProfile = () => {
   return (
-    <div>VenderProfile</div>
+    <>
+    <div className='vender-profile'>
+        <div className='vender-profile-img'>
+            <img src="/Home/comp.jpeg" alt=""/>
+            </div>
+         <div className='vender-profile-info'>
+                <h1>Company Name</h1>
+                <p>Company description</p>
+        </div>
+
+       </div>     
+    </>
   )
 }
 
