5ee73d5f59d74693415b7d6a9d20abd9b16c18f6
IshKevin <carterk279@gmail.com>
2023-03-28 21:58:01 +0200

    add vendele profile scss

--- src/Components/Singlevender/Venderprofile/VenderProfile.js
+++ src/Components/Singlevender/Venderprofile/VenderProfile.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import './VenderProfile.css'
 
 const VenderProfile = () => {
   return (
     <>
     <div className='vender-profile'>
         <div className='vender-profile-img'>
-            <img src="/Home/comp.jpeg" alt=""/>
-            </div>
-         <div className='vender-profile-info'>
-                <h1>Company Name</h1>
-                <p>Company description</p>
+        <img src={'Home/jordan.jpg'} alt=""/>
+        </div>
+        <div className='vender-profile-info'>
+         <h1>UMUSHANANA SHOP </h1>
+         <p>UMUSHANANA is the company located in huye that is used to sold </p>
         </div>
 
        </div>     
