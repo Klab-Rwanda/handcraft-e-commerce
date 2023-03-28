366be0f055985909f90ad78500298ba5ba7cc91f
IshKevin <carterk279@gmail.com>
2023-03-28 21:27:46 +0200

    add vender profile

---
+++ src/Components/Singlevender/Venderprofile/VenderProfile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VenderProfile = () => {
+  return (
+    <div>VenderProfile</div>
+  )
+}
+
+export default VenderProfile
--- src/Pages/SingleVender/Singlevender.js
+++ src/Pages/SingleVender/Singlevender.js
@@ -4,6 +4,7 @@ import Header from '../../Components/Home/Header/Header'
 import SubHeader from '../../Components/Home/SubHeader/SubHeader'
 import Footer from '../../Components/Home/Footer/Footer'
 import CategoliesNav from '../../Components/CategoliesNav/CategoliesNav'
+import VenderProfile from '../../Components/Singlevender/Venderprofile/VenderProfile'
 
 
 const Singlevender = () => {
@@ -12,7 +13,10 @@ const Singlevender = () => {
       <Header />
       <SubHeader />
       <div className='Singlevender-body'>
-      <div> <CategoliesNav/> </div>
+      <div>
+         <VenderProfile/>
+         <CategoliesNav/> 
+         </div>
        <div> right side</div>
        </div>
        <Footer/>
