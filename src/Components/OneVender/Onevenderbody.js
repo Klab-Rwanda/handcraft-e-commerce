287b558ce825d7df203d64ba64b6429fdef5f773
IshKevin <carterk279@gmail.com>
2023-03-31 09:42:19 +0200

    fix onevender body

---
+++ src/Components/OneVender/Onevenderbody.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Onevenderbody = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Onevenderbody
--- src/Pages/OneVender/OneVender.js
+++ src/Pages/OneVender/OneVender.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import Header from '../../Components/Home/Header/Header'
+import SubHeader from '../../Components/Home/SubHeader/SubHeader'
+import Footer from '../../Components/Home/Footer/Footer'
+import Shopdata from '../../Components/Shop/Shopdata/Shopdata'
+import CategoliesNav from '../../Components/CategoliesNav/CategoliesNav'
+import './Onevenderbody.css'
+import Shopbanner from '../../Components/Shop/Shopbanner/Shopbanner'
+import Hotitems from '../../Components/Shop/Hotitems/Hotitems'
 
 const OneVender = () => {
   return (
-    <div>OneVender</div>
+    <div><Header/>
+    <SubHeader/>
+    <div className="OneVender-container">
+     <div className='OneVender-cat1'>
+      <CategoliesNav/>
+      <Hotitems/>
+      </div> 
+      <div className='Onevender-cat2'>
+        <h1 className='single-vender-name'> [Name of vender ]  List of Product </h1>
+        <Shopdata className='vender-view'/>
+        </div>
+    </div>
+    <Footer/></div>
   )
 }
 
