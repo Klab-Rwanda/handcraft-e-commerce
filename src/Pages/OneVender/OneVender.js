e54c1e47f824ed14ff5fd16dd227744c3e7342cc
IshKevin <carterk279@gmail.com>
2023-03-31 10:11:11 +0200

    one vendre view

--- src/Pages/OneVender/OneVender.js
+++ src/Pages/OneVender/OneVender.js
@@ -5,7 +5,7 @@ import Footer from '../../Components/Home/Footer/Footer'
 import Shopdata from '../../Components/Shop/Shopdata/Shopdata'
 import CategoliesNav from '../../Components/CategoliesNav/CategoliesNav'
 import './Onevenderbody.css'
-import Shopbanner from '../../Components/Shop/Shopbanner/Shopbanner'
+import Onevenderbody from '../../Components/OneVender/Onevenderbody'
 import Hotitems from '../../Components/Shop/Hotitems/Hotitems'
 
 const OneVender = () => {
@@ -20,6 +20,7 @@ const OneVender = () => {
       <div className='Onevender-cat2'>
         <h1 className='single-vender-name'> [Name of vender ]  List of Product </h1>
         <Shopdata className='vender-view'/>
+        <Onevenderbody/>
         </div>
     </div>
     <Footer/></div>
